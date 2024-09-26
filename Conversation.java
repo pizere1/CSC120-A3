68c5108346dc0e74ce0ab0b23542464e756bd475
Peace Izere <pizere@smith.edu>
2024-09-25 23:52:05 -0400

    Update Conversation.java

--- Conversation.java
+++ Conversation.java
@@ -15,7 +15,9 @@ public class WordCheckers {
         String array3[] = {"i","am","i'm", "you're", "my", "your", "We"};
         String array4[] = {"you","are","you're", "i'm", "your", "my", "you"};
         int mod=Number%9;
+        //As the first and second strings in array1[] will appear once, the rest of the array has 9 canned responses which will be repeated with respect to the needs of the user
         int rounds= (Number-mod)/9;
+            /*if the rounds desired are less than the size of array 1, the chatbot will simply go through the defined response once*/
         if (Number<array1.length){
             for (int k = 0; k <= Number; k++) {
                 System.out.println(array1[k]);
@@ -31,8 +33,11 @@ public class WordCheckers {
                     k++;}
                 }
             }}
+        /*if the desired rounds are big than array1, then execution will happen in 3 parts*/
         else {
-            for (int k = 0; k < array1.length; k++) {
+                /*First one: The chatbot goes through the canned response once*/
+            for (int k = 0; k < array1.length; k++) 
+            {
                 System.out.println(array1[k]);
                 Output.add(array1[k]);
                 sentence=input2.nextLine();
@@ -46,7 +51,9 @@ public class WordCheckers {
                         k++;}
                 }
             }
-            for (i = 1; i < rounds; i++) {
+                //Once done with the first round, the chatbot repeats for more rounds which are equal to one less than the calculated times in line 18
+            for (i = 1; i < rounds; i++)
+                    {
                 for (int j = 2; j < array1.length; j++) {
                     Output.add(array1[j]);
                     System.out.println(array1[j]);
@@ -62,8 +69,11 @@ public class WordCheckers {
                     }
                 }
             }
+        //Lastly, it runs through the remaining rounds, i.e if the number was calculated in line 17
             if (mod != 0) {
-                for (int j = 1; j < mod; j++) {
+                for (int j = 1; j < mod; j++) 
+        //1 less than the calculated modulus as the very first round had 10 conversation lines not 9 as planned for rounds
+                {
                     Output.add(array1[j + 1]);
                     System.out.println(array1[j + 1]);
                     sentence=input2.nextLine();
