a944224a32dc8c7485c9b3cce8dbd0337415c551
Peace Izere <pizere@smith.edu>
2024-11-04 18:06:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-/*all my codes are in conversation.java*/
-public class Main {
-  public static void main(String[] args) {
-    /* the next line will run your Conversation bot */
-    Conversation.main(args);
-  }
-}
+public static void main(String[] args) {
+        Conversation conversations = new Conversation();
+    }
